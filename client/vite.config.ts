0761a896fe97b203fc231c3121d08fff0ca5a2f8
User <user@example.com>
2025-09-16 15:16:56 +0530

    upgrade

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import redirectsPlugin from './vite-redirects-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -23,7 +24,10 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      redirectsPlugin()
+    ],
     resolve: {
       alias: {
         '@': resolve(__dirname, './src'),
